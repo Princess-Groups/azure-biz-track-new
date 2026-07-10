489485cae977da16a0d20d267deaeeffa4087837
Srinesh <srineshs118@gmail.com>
2026-07-10 11:24:13 +0530

    Remove all files

--- supabase/migrations/20260623153608_4706934f-f80b-44f2-b358-f543259f6f3f.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO public.expense_categories (name, icon, color) VALUES
-  ('Books', 'book', '#10B981'),
-  ('Stationery', 'pencil-ruler', '#14B8A6'),
-  ('Coffee', 'coffee', '#92400E'),
-  ('Snacks', 'cookie', '#EAB308'),
-  ('Food', 'utensils', '#EF4444')
-ON CONFLICT (name) DO UPDATE SET icon = EXCLUDED.icon, color = EXCLUDED.color;
