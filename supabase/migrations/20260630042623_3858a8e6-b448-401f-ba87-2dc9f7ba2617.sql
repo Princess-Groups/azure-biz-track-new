ddcf14a76caf73471f6b5559189aeece993dfd46
Srinesh <srineshs118@gmail.com>
2026-07-11 20:03:29 +0530

    Remove all files

--- supabase/migrations/20260630042623_3858a8e6-b448-401f-ba87-2dc9f7ba2617.sql
+++
@@ -1,6 +0,0 @@
-
-DO $$ BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'staff' AND enumtypid = 'public.app_role'::regtype) THEN
-    ALTER TYPE public.app_role ADD VALUE 'staff';
-  END IF;
-END $$;
